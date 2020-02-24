be2a042efad826a1fa0a1e1ad8bc5f67ca1bc7ff
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-02-24 09:30:09 -0400

    Agregando Models Pagos

---
+++ SistemaInscripcion/Models/Pagos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion.Models
+{
+    public class Pagos
+    {
+        [Key]
+        public int PagoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int InscripcionId { get; set; }
+        public int Monto { get; set; }
+        public Pagos()
+        {
+            PagoId = 0;
+            Fecha = DateTime.Now;
+            InscripcionId = 0;
+            Monto = 0;
+        }
+    }
+}
