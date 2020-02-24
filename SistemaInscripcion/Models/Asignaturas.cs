512d301547d076899470fcfa55853267414bb947
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-02-24 09:25:04 -0400

    Creando Models Asignaturas

---
+++ SistemaInscripcion/Models/Asignaturas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion.Models
+{
+    public class Asignaturas
+    {
+        [Key]
+        public int AsignaturaId { get; set; }
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string PreRequisito { get; set; }
+        public int Creditos { get; set; }
+        public Asignaturas()
+        {
+            AsignaturaId = 0;
+            Codigo = 0;
+            Descripcion = string.Empty;
+            PreRequisito = string.Empty;
+            Creditos = 0;
+        }
+    }
+}
