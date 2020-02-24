009e5fd92df49bb2f58f06fae41a978bfb8eb9b1
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-02-24 08:12:09 -0400

    instalando entityframework

---
+++ SistemaInscripcion/Data/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion.Data
+{
+    public class Contexto
+    {
+    }
+}
