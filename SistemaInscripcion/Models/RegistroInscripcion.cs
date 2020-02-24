4e02fbb7ddb59800cbf3b9caf4d77aaadbcdf29a
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-02-24 09:27:33 -0400

    Creando Models RegistroInscripcion

---
+++ SistemaInscripcion/Models/RegistroInscripcion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion.Models
+{
+    public class RegistroInscripcion
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        public string Matricula { get; set; }
+        public string Nombres { get; set; }
+        public int Balance { get; set; }
+        public RegistroInscripcion()
+        {
+            EstudianteId = 0;
+            Matricula = string.Empty;
+            Nombres = string.Empty;
+            Balance = 0;
+        }
+    }
+}
