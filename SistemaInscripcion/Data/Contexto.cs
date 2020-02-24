edcd97772f7dc6bce26f955067b839b23ad83c39
Leonardo Emil <48894730+lean56@users.noreply.github.com>
2020-02-24 08:19:08 -0400

    creando models inscripcion

--- SistemaInscripcion/Data/Contexto.cs
+++ SistemaInscripcion/Data/Contexto.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaInscripcion.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SistemaInscripcion.Data
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<Inscripcion> Inscripcion { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Database/InscripcionDb.db");
+        }
     }
 }
---
+++ SistemaInscripcion/Models/Inscripcion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion.Models
+{
+    public class Inscripcion
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        [Required(ErrorMessage = "El Semestre es Obligatorio!!!")]
+        public int EstudianteId { get; set; }
+
+        public string Semestre { get; set; }
+        [Range(minimum: 1, maximum: 28, ErrorMessage = "El limite de creditos es 28")]
+        public int Limite { get; set; }
+        [Range(minimum: 1, maximum: 28, ErrorMessage = "El limite de creditos tomado es 28")]
+        public int Tomados { get; set; }
+        public int Disponible { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Monto { get; set; }
+        public int Balance { get; set; }
+
+        public Inscripcion()
+        {
+            InscripcionId = 0;
+            Semestre = string.Empty;
+            Limite = 0;
+            Tomados = 0;
+            Disponible = 0;
+            EstudianteId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+            Balance = 0;
+        }
+    }
+}
